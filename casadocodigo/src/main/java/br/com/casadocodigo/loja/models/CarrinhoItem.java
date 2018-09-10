c4bfac8d0d91b013e235c4df65c2e8f093342a19
graziele.vasconcelos <grazisbvasconcelos@gmail.com>
2018-09-10 00:04:38 -0300

    Implementação da pagina de carrinho com os totais relevantes

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoItem.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoItem.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
+
 public class CarrinhoItem {
 
 	private Produto produto;
@@ -54,4 +56,11 @@ public class CarrinhoItem {
 		return true;
 	}
 
+	public BigDecimal getPreco() {
+		return produto.precoPara(tipoPreco);
+	}
+
+	public BigDecimal getTotal(int quantidade) {
+		return this.getPreco().multiply(new BigDecimal(quantidade));
+	}
 }
